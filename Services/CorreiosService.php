4e90f45bf0d2e2c30a775da44c6e017ea06085e3
Artur <nezkal@gmail.com>
2016-03-31 09:48:04 -0300

    Fix URL

--- Services/CorreiosService.php
+++ Services/CorreiosService.php
@@ -222,8 +222,11 @@ class CorreiosService implements ServicesInterface
 
         $error = false;
 
-        $xml = file_get_contents($url);
-
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $xml = curl_exec($ch);
+        curl_close($ch);
 
         $simplexml = new \SimpleXMLElement($xml);
 
